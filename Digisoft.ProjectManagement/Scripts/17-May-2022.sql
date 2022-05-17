1ba8ace38c8d2fe2b5ea2f663db66eb19b124bf1
Nipun Tripathi <nipun.tripathi@dewmaker.online>
2022-05-17 14:50:50 +0530

    script

---
+++ Digisoft.ProjectManagement/Scripts/17-May-2022.sql
@@ -0,0 +1,6 @@
+Alter Table UserDetail Add Vaccination2ndDoseDate date NULL
+Alter Table UserDetail Add Vaccination1stDoseDate date NULL
+Alter Table UserDetail Add BloodGroup NVARCHAR(10) NULL
+Alter Table UserDetail Add MarritalStatus NVARCHAR(20) NULL
+Alter Table UserDetail Add MotherName NVARCHAR(120) NULL
+Alter Table UserDetail Add FatherName NVARCHAR(120) NULL
